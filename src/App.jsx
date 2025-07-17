2e9953c4d48e1c5e4c9d87d1f89dedb457465bf0
Rodrigo <rodrigoale99@outlook.com>
2025-07-17 18:19:32 -0400

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React,{ Fragment} from "react" 
+import { TodoList } from "./components/TodoList"
+
+export function App(){
+    return (
+        <Fragment>
+            <div className="container mt-3">
+            <TodoList/>
+            </div>
+        </Fragment>
+    )
+}
+
